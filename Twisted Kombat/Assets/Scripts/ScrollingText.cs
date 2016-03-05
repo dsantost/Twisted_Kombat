c0420282ba04e6e4cb08cb4a40a5b538336d0dad
Diogo Ribeiro <diogo_p_ribeiro@hotmail.com>
2016-03-05 17:42:40 +0000

    Intro scene

---
+++ Twisted Kombat/Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingText : MonoBehaviour {
+	private GameObject introText;
+
+	// Use this for initialization
+	void Start () {
+		introText = GameObject.FindGameObjectWithTag ("ScrollingText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		introText.transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
+		//introText.transform.localPosition.y += 1 * Time.deltaTime;
+	}
+}
